f5059ed56a1bae174bad2591df8d1feea6a4b7e1
Carlos Valderrama <carlosValderrama@gmail.com>
2025-02-08 11:02:51 -0800

    Initial Commit

---
+++ CarlosVTests/CarlosVTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarlosVTests.swift
+//  CarlosVTests
+//
+//  Created by Carlos Valderrama on 2/8/25.
+//
+
+import Testing
+@testable import CarlosV
+
+struct CarlosVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
